1c3fa59983854739664a64ab612800c46e4a7823
axshish <133031616+axshish@users.noreply.github.com>
2024-11-05 09:59:12 +0530

    Add files via upload

---
+++ testbank.sol
@@ -0,0 +1,30 @@
+//SPDX-License-Identifier: MIT 
+
+pragma solidity ^0.8.0;
+
+
+contract bank{
+
+    mapping(address => uint) public balances;
+
+    function deposit() public payable{
+        balances[msg.sender] += msg.value; 
+    }
+    
+    function withdraw(uint _amount) public{
+
+        require(balances[msg.sender]>= _amount, "Not enough ether");
+
+        balances[msg.sender] -= _amount*1000000000000000000;
+
+        (bool sent,) = msg.sender.call{value: _amount*1000000000000000000}("Sent");
+        require(sent, "failed to send ETH");
+
+        
+    }
+
+    function getBal() public view returns(uint){
+        return address(this).balance/1000000000000000000;
+    }
+
+}
